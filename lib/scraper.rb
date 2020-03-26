962e6f0377bb98b54d8b96029587c397dcf5e1f0
Brian Vehige <brianvehige@gmail.com>
2020-03-26 23:47:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open)
+    doc = Nokogiri::HTML(open())
   end
 
   def self.scrape_profile_page(profile_url)
