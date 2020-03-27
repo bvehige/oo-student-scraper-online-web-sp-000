c25766c627049024793957db6a52ceff5c889385
Brian Vehige <brianvehige@gmail.com>
2020-03-27 00:13:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,9 +6,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html = open(index_url)
-    
-    
-    Nokogiri::HTML(open(index_url)
+    Nokogiri::HTML(html)
     
     binding.pry
   end
