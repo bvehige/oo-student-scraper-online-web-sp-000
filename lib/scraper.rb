f59593025323ed19f3443af7d788c3a5c30b042c
Brian Vehige <brianvehige@gmail.com>
2020-03-27 00:55:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,8 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     students = []
-    
+    doc.css("div.student-card").each do |student|
+      name = student.css("")
   end
 Scraper.new.scrape_index_page(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html)
 
