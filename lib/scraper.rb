dfc46ca88fe9081ac4f00533594f40e673821859
Brian Vehige <brianvehige@gmail.com>
2020-03-27 01:22:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,7 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
-    students = {}
+    student = {}
     
     social_icon_container = page.css(".social_icon_container a").collect {|icon|icon.attribute("href").value}
     social_icon_container.each do |link|
