854116503c9e6d5075d586d3939a9f37739e5ada
Brian Vehige <brianvehige@gmail.com>
2020-03-27 01:00:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,12 @@ class Scraper
     doc.css("div.student-card").each do |student|
       name = student.css(".student-name").text 
       location = student.css(".student-location").text
-      profile_url = student.css()
+      profile_url = student.css("a").attribute("href").value
+      student_info = {:name => name, :location => location, :profile_url => profile_url)
+      students << student_info
+    end
+    students
+      
   end
 Scraper.new.scrape_index_page(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html)
 
