f2e5af008663d5925537ca28a4c644d855704442
Brian Vehige <brianvehige@gmail.com>
2020-03-27 01:53:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,12 +5,12 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
-  
+    student_hash.each {|k, v| self.send(("#{k}="), v)}
     @@all << self 
   end
 
   def self.create_from_collection(students_array)
+    students_array.each {
     
   end
 
